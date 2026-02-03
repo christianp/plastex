ef9f46f046f0b6879ab2b2092bec9ea5cabfa8fc
Christian Lawson-Perfect <christian@lawson-perfect.uk>
2026-02-03 11:16:24 +0000

    fix includegraphics when given an exact filename in a graphicspath
    The loop to check files in each of the registered graphicspath was only looking at files with a `.` character after the given filename, so if the author gave the full filename it wasn't found.
    
    This changes the glob to look for any file with the given string as a prefix.

--- plasTeX/Packages/graphicx.py
+++ plasTeX/Packages/graphicx.py
@@ -24,7 +24,7 @@ class includegraphics(Command):
 
         # Check for file using graphicspath
         for p in paths:
-            for fp in Path(p).glob(f+'.*'):
+            for fp in Path(p).glob(f+'*'):
                 if fp.is_file() and fp.suffix.lower() in ext and fp.stem==Path(f).stem:
                     img = str(fp.resolve())
                     break
